95bb8f45242c8ccee2dd3bc5cb85c80cedafbfee
mkozar <kozarmisha@mail.ru>
2022-08-03 11:39:27 +0300

    создал компонент Footer. Добавил созданный комнонент на сайт.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Footer.css';
+import GitHub from '../../assets/github.png';
+import Instagram from '../../assets/instagram.png';
+import LinkedId from '../../assets/linkedin.png';
+import Logo from '../../assets/logo.png';
+
+const Footer = () => {
+    return (
+        <div className="footer__container">
+            <div className="footer">
+                <div className="footer__social">
+                    <img src={GitHub} alt="" />
+                    <img src={Instagram} alt="" />
+                    <img src={LinkedId} alt="" />
+                </div>
+
+                <div className="footer__logo">
+                    <img src={Logo} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
